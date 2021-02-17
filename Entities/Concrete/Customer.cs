51894b8984c9e1a8f1e5bbfd0b476ce6ea31faa4
cigkofteci <cigkofteci@github.com>
2021-02-17 19:06:27 +0300

    Users-->Id,FirstName,LastName,Email,Password Customers-->UserId,CompanyName Rentals-->Id, CarId, CustomerId, RentDate, ReturnDate Entity'leri oluşturuldu.

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Müşteriler tablosu
+    public class Customer:IEntity
+    {
+        //Customers-->UserId,CompanyName
+
+        public int Id { get; set; }
+        public int UserId { get; set; } //*****Kullanıcılar ve müşteriler ilişkilidir.
+        public string CompanyName { get; set; }
+
+    }
+}
---
+++ Entities/Concrete/Rentals.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Arabanın kiralanma bilgisini tutan tablo
+    public class Rentals:IEntity
+    {
+        //Rentals-->Id, CarId, CustomerId, RentDate(Kiralama Tarihi), ReturnDate(Teslim Tarihi)
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+    }
+}
