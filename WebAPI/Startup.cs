08d37d5828c54107ed092d52b7a4ebe7547083c7
cigkofteci <cigkofteci@github.com>
2021-02-23 10:02:59 +0300

    CustomerController eklendi

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -36,6 +36,9 @@ namespace WebAPI
 
             services.AddSingleton<IUserService, UserManager>();
             services.AddSingleton<IUserDal, EfUserDal>();
+            
+            services.AddSingleton<ICustomerService, CustomerManager>();
+            services.AddSingleton<ICustomerDal, EfCustomerDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
