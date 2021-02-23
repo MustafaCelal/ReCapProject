27f978cf701649f983307c0704c20f06d9c03049
cigkofteci <cigkofteci@github.com>
2021-02-23 10:24:17 +0300

    RentalsController eklendi

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -39,6 +39,9 @@ namespace WebAPI
             
             services.AddSingleton<ICustomerService, CustomerManager>();
             services.AddSingleton<ICustomerDal, EfCustomerDal>();
+
+            services.AddSingleton<IRentalService, RentalManager>();
+            services.AddSingleton<IRentalDal, EfRentalDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
